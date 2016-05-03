295a6e510dac01ac46d038209e42b9c3caf47b45
Christian <Christian@Christian-PC>
2016-05-03 08:59:21 -0500

    modificadoR_holaM

--- burgosSuma/src/burgossuma/BurgosSuma.java
+++ burgosSuma/src/burgossuma/BurgosSuma.java
@@ -15,6 +15,7 @@ public class BurgosSuma {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("soy chistian");
         // TODO code application logic here
     }
     
